b6c9ebca23c3ce9bbfb897c3e70b8a74c657b19f
weammakhoul <73224927+weammakhoul@users.noreply.github.com>
2021-11-19 19:20:51 +0200

    Update kmeans.py

--- kmeans.py
+++ kmeans.py
@@ -1,2 +1,6 @@
-print("nader")
-print ("weam")
+def kmeans (k , inputfile , outputfile , max_iter = 200):
+    assert k is int , "Invalid inpit."
+    assert max_iter is int , "Invalid inpit."
+    
+    return 0
+
