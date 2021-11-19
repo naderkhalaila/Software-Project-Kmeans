154c663cba749fea582c986c05a048857e5403b1
naderkhalaila <82514692+naderkhalaila@users.noreply.github.com>
2021-11-19 22:04:11 +0200

    Update kmeans.py

--- kmeans.py
+++ kmeans.py
@@ -3,7 +3,15 @@ def kmeans (k ,max_iter = 200, inputfile=None , outputfile=None):
     assert type(k) == int , "Invalid input."
     assert type(max_iter) == int , "Invalid input."
     
-    file = open(outputfile)
-    print(file.read())
+    file = open(inputfile)
+    content = file.read()
+    lst = content.splitlines()
+    for i in range(len(lst)):
+        lst[i] = lst[i].split(",")
+        for j in range(len(lst[i])):
+            lst[i][j] = float(lst[i][j])
     
     return 0
+
+
+
