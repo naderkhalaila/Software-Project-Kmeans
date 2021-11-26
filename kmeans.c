1c3970358335d99f4bfaee2b3bb6afa09afc7377
weammakhoul <73224927+weammakhoul@users.noreply.github.com>
2021-11-26 13:16:20 +0200

    Update kmeans.c

--- kmeans.c
+++ kmeans.c
@@ -2,6 +2,13 @@
 
 int main() {
 
+   int array_length = 100;
+    int *array = (int*) malloc(array_length * sizeof(int));;
+    printf("%d\n", sizeof(array));
+    for(int i=0 ; i< sizeof(array) ; i++){
+        printf("%f\n",array[i] );
+    }
+   
    // FILE *in_file = fopen("C:\\Users\\nadsa\\Documents\\input_3.txt", "r");
     FILE *f;
     char c;
