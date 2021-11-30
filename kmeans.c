231fd8d2138a8a6cad484d78ba44fc8cda8e1f94
naderkhalaila <82514692+naderkhalaila@users.noreply.github.com>
2021-11-30 15:11:55 +0200

    Update kmeans.c

--- kmeans.c
+++ kmeans.c
@@ -1,24 +1,70 @@
 #include <stdio.h>
+#include <malloc.h>
+#include <math.h>
 
 int main() {
 
-   int array_length = 100;
-    int *array = (int*) malloc(array_length * sizeof(int));;
-    printf("%d\n", sizeof(array));
-    for(int i=0 ; i< sizeof(array) ; i++){
-        printf("%f\n",array[i] );
-    }
-   
-   // FILE *in_file = fopen("C:\\Users\\nadsa\\Documents\\input_3.txt", "r");
+
+
+    // FILE *in_file = fopen("C:\\Users\\nadsa\\Documents\\input_3.txt", "r");
     FILE *f;
     char c;
-    f=fopen("C:\\Users\\nadsa\\Documents\\input_3.txt", "rt");
+    f=fopen("C:\\Users\\nadsa\\Documents\\input_1.txt", "rt");
 
-
+    int cnt1 = 0;
+    int cnt2 = 0;
     while((c=fgetc(f))!=EOF){
-        printf("%c",c);
+        if(c==','){
+            cnt1++;
+        }
+        if(c=='\n'){
+            cnt2++;
+        }
+        //printf("%c",c);
     }
-
     fclose(f);
+    //printf("%d, %d",cnt1,cnt2);
+    double *arr[cnt1];
+    for(int i=0 ; i<cnt2 ; i++){
+        arr[i] = (double*)malloc((cnt1/cnt2 +1)* sizeof(double));
+    }
+    FILE *file;
+    char C;
+    file=fopen("C:\\Users\\nadsa\\Documents\\input_1.txt", "rt");
+    for(int i=0 ; i<cnt2 ; i++){
+        for(int j=0 ; j<cnt1/cnt2 +1 ; j++){
+            double num = 1;
+            int negative = 0;
+            while((C=fgetc(file))!='.' && C!=EOF){
+                if(C=='-'){
+                    negative = 1;
+                }
+                else{
+                    num = num * ((double)C - 48);
+                }
+
+            }
+            double fr = 0;
+            int p = 1;
+            while((C=fgetc(file))!=EOF && C!=',' && C!='\n'){
+                fr = fr + ((double)C - 48) * pow(10 , -1*p);
+                p++;
+            }
+            num = num + fr;
+            if(negative == 1){
+                num = -1*num;
+            }
+            arr[i][j] = num;
+        }
+    }
+    fclose(file);
+
+    for(int i=0 ; i<cnt2 ; i++) {
+        for (int j = 0; j < cnt1/cnt2 + 1; j++) {
+            printf("%f", arr[i][j]);
+            printf(",");
+        }
+        printf("\n");
+    }
     return 0;
 }
