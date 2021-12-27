d0f9c08037a650c4bba8fb722632ceaf726f768b
naderkhalaila <82514692+naderkhalaila@users.noreply.github.com>
2021-12-27 11:48:18 +0200

    Update kmeans_pp.py

--- kmeans_pp.py
+++ kmeans_pp.py
@@ -1,3 +1,9 @@
+import pandas as pd
+import numpy as np
+
+names = ["index"]
+DataPoints = pd.read_csv(r'C:\Users\nadsa\Documents\SW-project\input_1_db_2.txt' , names =names + ["dim" + str(i) for i in range(1,5)])
+print(DataPoints)
 def dist(point1, point2):
     sum = 0
     d = len(point1)
@@ -15,4 +21,25 @@ def mindist(centroids_list, point, i):
             min = distance
             index = j
     return index
+
+def kmeansPlus(k,max_iter, epsilon , filename1, filename2):
+    centroids = []
+    centroids_index=[]
+
+
+
+def init(k, filename1 , filename2 , centroids, centroids_index):
+
+    DataPoints = pd.read_csv(filename1,
+                             names=names + ["dim" + str(i) for i in range(k)])
+    DataPoints["D"] = [0 for i in range(len(DataPoints))]
+    DataPoints["P"] = [0 for i in range(len(DataPoints))]
+
+    np.random.seed(0)
+    muo = np.random.choice(len(DataPoints), 1)
+
+    i=1
+    while(i<k):
+        muo = np.random.choice(len(DataPoints) , 1, p=DataPoints['P'].to_numpy())
+
 
